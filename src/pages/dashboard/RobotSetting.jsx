c3995c2555106ecb2f97f3c1921a1e939a531741
Hodayousry2752 <hanaayoussrey@gmail.com>
2025-10-26 22:05:49 +0300

    reDeploy

--- src/pages/dashboard/RobotSetting.jsx
+++ src/pages/dashboard/RobotSetting.jsx
@@ -84,6 +84,7 @@ export default function RobotSettings({ robots }) {
                 </Tooltip>
               ))}
             </TooltipProvider>
+            
           </div>
         </motion.div>
 
